4723ba65f18ee536e37153f975118333e3c9463f
Somdeep Sardar <sardarsomdeep123@gmail.com>
2024-04-22 12:09:40 +0530

    ingestion updated

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -87,21 +87,21 @@ class DataIngestion:
         except Exception as e:
             raise CustomException(e,sys)
         
-# if __name__ == "__main__":
-#     logging.info("Execution has started")
+if __name__ == "__main__":
+    logging.info("Execution has started")
 
-#     try:
-#         # data_ingestion_config = DataIngestionConfig()
-#         data_ingestion = DataIngestion()
-#         train_data_path, test_data_path = data_ingestion.initiate_data_ingestion()
+    try:
+        # data_ingestion_config = DataIngestionConfig()
+        data_ingestion = DataIngestion()
+        train_data_path, test_data_path = data_ingestion.initiate_data_ingestion()
 
-#         data_transformation = DataTransformation()
-#         train_arr, test_arr,_ = data_transformation.initiate_data_transformation(train_data_path, test_data_path)
+        data_transformation = DataTransformation()
+        train_arr, test_arr,_ = data_transformation.initiate_data_transformation(train_data_path, test_data_path)
 
-#         model_trainer = ModelTrainer()
-#         model_trainer.initiate_model_trainer(train_arr, test_arr)
+        model_trainer = ModelTrainer()
+        model_trainer.initiate_model_trainer(train_arr, test_arr)
         
-#     # Get the configuration for data ingestion
-#     except Exception as e:
-#         logging.info("custom exception raised in app.py")
-#         raise CustomException(e, sys)
+    # Get the configuration for data ingestion
+    except Exception as e:
+        logging.info("custom exception raised in app.py")
+        raise CustomException(e, sys)
